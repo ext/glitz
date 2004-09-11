313f15c9d267ca99707f1700c5b30dc1a0b30ef9
David Reveman <davidr@novell.com>
2004-09-11 14:52:49 +0000

    Bump version to 0.2.2

--- src/glitz.h
+++ src/glitz.h
@@ -39,7 +39,7 @@
 /* NOTE: Must be manually synchronized with GLITZ_VERSION in configure.in */
 #define GLITZ_MAJOR 0
 #define GLITZ_MINOR 2
-#define GLITZ_REVISION 1
+#define GLITZ_REVISION 2
 
 #if defined(__cplusplus) || defined(c_plusplus)
 extern "C" {
